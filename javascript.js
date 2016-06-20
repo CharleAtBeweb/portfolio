df3fb1ebbecc5551161782d2aaf438e718a20549
charles-andre <lunelfrance@gmail.com>
2016-06-20 14:54:39 +0200

    sauvegarde lundi 20 juin 14h54

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+window.resize = function()
+{
+
+var head = document.getElementById('header');
+head.style.width = '50%';
+
+
+}
